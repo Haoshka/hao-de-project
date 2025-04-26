269121805185cc7f7d3bedd4a093548fa4370981
Olga <kabalkina66@gmail.com>
2025-04-26 09:12:09 +0200

    add terraform variables

---
+++ 1.Terraform/variables.tf
@@ -0,0 +1,35 @@
+
+
+variable "project" {
+  description = "Project"
+  default     = "hao-de-project"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "europe-west1-b"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "europe-west1"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "hao_project_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "hao_project_bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
