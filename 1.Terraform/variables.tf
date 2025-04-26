f2910a121ac9afbb0b1d3f6e38964c822a86c546
Olga <kabalkina66@gmail.com>
2025-04-26 09:48:39 +0200

    modify variable.tf

--- 1.Terraform/variables.tf
+++ 1.Terraform/variables.tf
@@ -20,13 +20,13 @@ variable "location" {
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default     = "hao_project_dataset"
+  default     = "hao-de-project-dataset"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "hao_project_bucket"
+  default     = "hao-de-project-bucket"
 }
 
 variable "gcs_storage_class" {
